028fdc82850863b2b2ae8c6bd65a08a7eaaa2ea8
Nagababu Thota <nagababu.t@technovert.com>
2023-11-28 10:54:26 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using namespace GitDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            SayHello();
+        }
+
+        public void static SayHello(){
+            Console.Writeline("Hello there!");
+        }
+    }
+}
